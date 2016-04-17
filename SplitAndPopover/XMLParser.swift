d3cb1721227e9c8743fcd6b5e305a03a828f23fd
swillsea <sam.willsea@gmail.com>
2016-04-16 22:36:58 -0700

    first commit

---
+++ SplitAndPopover/XMLParser.swift
@@ -0,0 +1,13 @@
+//
+//  XMLParser.swift
+//  SplitAndPopover
+//
+//  Created by Sam on 4/16/16.
+//  Copyright © 2016 Sam Willsea. All rights reserved.
+//
+
+import UIKit
+
+class XMLParser: NSObject {
+
+}
